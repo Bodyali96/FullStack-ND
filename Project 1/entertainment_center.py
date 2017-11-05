340b855367e8487774d4520d03d64c0d9663143e
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-11-05 13:29:51 +0200

    Movie Trailer Website

--- Project 1/entertainment_center.py
+++ Project 1/entertainment_center.py
@@ -1,33 +1,61 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-# print(toy_story.storyline)
+thor_ragnarok = media.Movie("Thor: Ragnarok",
+                        "Imprisoned, the mighty Thor finds himself"
+                        "in a lethal gladiatorial contest against the Hulk,"
+                        "his former ally. Thor must fight for survival"
+                        "and race against time to prevent the all-powerful Hela"
+                        "from destroying his home and the Asgardian civilization.",
+                        "https://images-na.ssl-images-amazon.com/images/"
+                        "M/MV5BMjMyNDkzMzI1OF5BMl5BanBnXkFtZTgwODcxODg5MjI@."
+                        "_V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                        "https://www.youtube.com/watch?v=ue80QwXMRHg")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+jigsaw = media.Movie("Jigsaw",
+                     "Bodies are turning up around the city, each having met a uniquely gruesome"
+                     " demise. As the investigation proceeds, evidence points to one suspect:"
+                     " John Kramer, the man known as Jigsaw, who has been dead for ten years.",
+                     "https://images-na.ssl-images-amazon.com/images/"
+                     "M/MV5BNmRiZDM4ZmMtOTVjMi00YTNlLTkyNjMtMjI2OTAxNjgwMWM1XkEyXkFqcGdeQXVyMjMxOTE0ODA@."
+                     "_V1_SY1000_CR0,0,648,1000_AL_.jpg",
+                     "https://www.youtube.com/watch?v=vPP6aIw1vgY")
 # print(avatar.storyline)
 # avatar.show_trailer()
 
-school_of_rock = media.Movie("School of Rock", "Storyline",
-                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+blade_runner = media.Movie("Blade Runner 2049",
+                        "A young blade runner's discovery of a long-buried secret leads him"
+                        "to track down former blade runner Rick Deckard,"
+                        "who's been missing for thirty years.",
+                        "https://images-na.ssl-images-amazon.com/images/"
+                        "M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@."
+                        "_V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                        "https://www.youtube.com/watch?v=gCcx85zbxz4")
 
-ratatouille = media.Movie("Ratatouille", "Storyline",
-                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+only_the_brave = media.Movie("Only the Brave",
+                        "Based on the true story of the Granite Mountain Hotshots,"
+                        "a group of elite firefighters risk everything"
+                        "to protect a town from a historic wildfire.",
+                        "https://images-na.ssl-images-amazon.com/images/"
+                        "M/MV5BMTY5ODc5OTc2M15BMl5BanBnXkFtZTgwNjM0NjIzMzI@._V1_.jpg",
+                        "https://www.youtube.com/watch?v=EE_GY6zccqc")
 
-midnight_in_paris = media.Movie("Midnight in Paris", "Storyline",
-                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
+the_foreigner = media.Movie("The Foreigner",
+                            "A humble businessman with a buried past seeks justice"
+                            "when his daughter is killed in an act of terrorism."
+                            "A cat-and-mouse conflict ensues with a government official,"
+                            "whose past may hold clues to the killers' identities.",
+                            "https://images-na.ssl-images-amazon.com/images/"
+                            "M/MV5BM2RlMjcyMGQtZTU3OC00NGRlLWExMGEtYjU3ZjUyMDc0NWZmXkEyXkFqcGdeQXVyNTI4MzE4MDU@."
+                            "_V1_SY1000_SX675_AL_.jpg",
+                            "https://www.youtube.com/watch?v=33iuQu3UtjI")
 
-hunger_games = media.Movie("Hunger Games", "Storyline",
-                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+it = media.Movie("Hunger Games",
+                           "A group of bullied kids band together when a shapeshifting demon,"
+                           " taking the appearance of a clown, begins hunting children.",
+                           "https://images-na.ssl-images-amazon.com/images/"
+                           "M/MV5BOTE0NWEyNDYtYWI5MC00MWY0LTg1NDctZjAwMjkyMWJiNzk1XkEyXkFqcGdeQXVyNjk5NDA3OTk@."
+                           "_V1_SY1000_CR0,0,674,1000_AL_.jpg",
+                           "https://www.youtube.com/watch?v=hAUTdjf9rko")
+movies = [thor_ragnarok, jigsaw, blade_runner, only_the_brave, the_foreigner, it]
 fresh_tomatoes.open_movies_page(movies)
