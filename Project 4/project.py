839148decac44dbd5b13f66e920a56f92b77c282
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-12-07 19:16:47 +0200

    handling CRUD operations

--- Project 4/project.py
+++ Project 4/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item, User
@@ -32,9 +32,67 @@ def show_category_items(category_name):
 @app.route('/catalog/<string:category_name>/<string:item_name>')
 def show_item(category_name, item_name):
     categories = session.query(Category).all()
-    category = session.query(Category).filter(Category.name == category_name).one()
+    category = session.query(Category).filter_by(name=category_name).one()
     item = session.query(Item).filter_by(category_id=category.id, name=item_name).one()
     return render_template('item_card.html', categories=categories, category_name=category_name, item=item)
+
+
+@app.route('/catalog/<string:category_name>/new', methods=['GET', 'POST'])
+def create_item(category_name):
+    if request.method == 'POST':
+        item = Item(name=request.form['name'],
+                    description=request.form['description'],
+                    category_id=request.form['category'],
+                    user_id=request.form['user'])
+        session.add(item)
+        session.commit()
+        return redirect(url_for('show_category_items', category_name=category_name))
+    categories = session.query(Category).all()
+    return render_template('new_item.html', category_name=category_name, categories=categories)
+
+
+@app.route('/catalog/item/new', methods=['GET', 'POST'])
+def create_item_default():
+    if request.method == 'POST':
+        item = Item(name=request.form['name'],
+                    description=request.form['description'],
+                    category_id=request.form['category'],
+                    user_id=request.form['user'])
+        session.add(item)
+        session.commit()
+        return redirect(url_for('show_category_items', category_name=item['category']['name']))
+    categories = session.query(Category).all()
+    return render_template('new_item_home.html', categories=categories)
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit', methods=['GET', 'POST'])
+def edit_item(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    item_to_edit = session.query(Item).filter_by(category_id=category.id, name=item_name).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            item_to_edit.name = request.form['name']
+        if request.form['description']:
+            item_to_edit.description = request.form['description']
+        if request.form['category']:
+            item_to_edit.category_id = request.form['category']
+        session.add(item_to_edit)
+        session.commit()
+        return redirect(url_for('show_category_items', category_name=category_name))
+    categories = session.query(Category).all()
+    return render_template('edit_item.html', category_name=category_name, item_name=item_name, categories=categories, item=item_to_edit)
+
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/delete', methods=['GET', 'POST'])
+def delete_item(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    item_to_delete = session.query(Item).filter_by(category_id=category.id, name=item_name).one()
+    if request.method == 'POST':
+        session.delete(item_to_delete)
+        session.commit()
+        return redirect(url_for('show_category_items', category_name=category_name))
+    categories = session.query(Category).all()
+    return render_template('delete_item.html', category_name=category_name, item_name=item_name, categories=categories, item=item_to_delete)
 
 
 if __name__ == '__main__':
