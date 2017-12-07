1d7f5e41228ca88de711790db48a8ed2337d4df4
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-12-07 20:59:48 +0200

    fixing dropdown menu

--- Project 4/project.py
+++ Project 4/project.py
@@ -16,25 +16,22 @@ session = DBSession()
 @app.route('/')
 @app.route('/catalog')
 def show_catalog():
-    categories = session.query(Category).all()
     latest_items = session.query(Item).order_by(Item.id.desc()).limit(10)
-    return render_template('catalog.html', categories=categories, latest_items=latest_items)
+    return render_template('catalog.html', categories=categories(), latest_items=latest_items)
 
 
 @app.route('/catalog/<string:category_name>/Items')
 def show_category_items(category_name):
-    categories = session.query(Category).all()
     category = session.query(Category).filter_by(name=category_name).one()
     items = session.query(Item).filter_by(category_id=category.id).all()
-    return render_template('category_items.html', categories=categories, items=items, category_name=category_name)
+    return render_template('category_items.html', categories=categories(), items=items, category_name=category_name)
 
 
 @app.route('/catalog/<string:category_name>/<string:item_name>')
 def show_item(category_name, item_name):
-    categories = session.query(Category).all()
     category = session.query(Category).filter_by(name=category_name).one()
     item = session.query(Item).filter_by(category_id=category.id, name=item_name).one()
-    return render_template('item_card.html', categories=categories, category_name=category_name, item=item)
+    return render_template('item_card.html', categories=categories(), category_name=category_name, item=item)
 
 
 @app.route('/catalog/<string:category_name>/new', methods=['GET', 'POST'])
@@ -47,8 +44,7 @@ def create_item(category_name):
         session.add(item)
         session.commit()
         return redirect(url_for('show_category_items', category_name=category_name))
-    categories = session.query(Category).all()
-    return render_template('new_item.html', category_name=category_name, categories=categories)
+    return render_template('new_item.html', category_name=category_name, categories=categories())
 
 
 @app.route('/catalog/item/new', methods=['GET', 'POST'])
@@ -61,8 +57,7 @@ def create_item_default():
         session.add(item)
         session.commit()
         return redirect(url_for('show_category_items', category_name=item['category']['name']))
-    categories = session.query(Category).all()
-    return render_template('new_item_home.html', categories=categories)
+    return render_template('new_item_home.html', categories=categories())
 
 
 @app.route('/catalog/<string:category_name>/<string:item_name>/edit', methods=['GET', 'POST'])
@@ -79,8 +74,7 @@ def edit_item(category_name, item_name):
         session.add(item_to_edit)
         session.commit()
         return redirect(url_for('show_category_items', category_name=category_name))
-    categories = session.query(Category).all()
-    return render_template('edit_item.html', category_name=category_name, item_name=item_name, categories=categories, item=item_to_edit)
+    return render_template('edit_item.html', category_name=category_name, item_name=item_name, categories=categories(), item=item_to_edit)
 
 
 @app.route('/catalog/<string:category_name>/<string:item_name>/delete', methods=['GET', 'POST'])
@@ -91,16 +85,20 @@ def delete_item(category_name, item_name):
         session.delete(item_to_delete)
         session.commit()
         return redirect(url_for('show_category_items', category_name=category_name))
-    categories = session.query(Category).all()
-    return render_template('delete_item.html', category_name=category_name, item_name=item_name, categories=categories, item=item_to_delete)
+    return render_template('delete_item.html', category_name=category_name, item_name=item_name, categories=categories(), item=item_to_delete)
 
 
 @app.route('/catalog/find', methods=['GET', 'POST'])
 def find_items():
     if request.method == 'POST' and 'search' in request.form:
         items = session.query(Item).filter(Item.name.like('%'+request.form['search']+'%')).all()
-        return render_template('find_items.html', items=items)
+        return render_template('find_items.html', items=items, categories=categories())
     return redirect(url_for('show_catalog'))
+
+
+def categories():
+    categories_list = session.query(Category).all()
+    return categories_list
 
 
 if __name__ == '__main__':
